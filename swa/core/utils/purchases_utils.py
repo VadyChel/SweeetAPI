b84e45103ec547d6c90d0c1f4c174e65ba0f0fef
VadyChel <zyzel19@gmail.com>
2022-02-08 20:50:04 +0200

    Fix purchase_handler

--- swa/core/utils/purchases_utils.py
+++ swa/core/utils/purchases_utils.py
@@ -2,7 +2,7 @@ from swa import crud
 from swa.core.managers import QueueManager
 
 
-def purchases_handler(manager: QueueManager):
+async def purchases_handler(manager: QueueManager):
     for item in manager.queue:
         crud.add_user_purchase(
             db=item.db,
